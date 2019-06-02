1d3d397273017d433a228e5bad5d011f578818e6
Saurabh Rai <saurabh.rai@crecientech.com>
2019-06-02 13:42:17 +0530

    Angular material updated.
    Angular material updated.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedmoduleModule } from '../sharedmodule/sharedmodule.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 const routes: Routes = [
@@ -14,7 +16,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedmoduleModule,
+    MatFormFieldModule
   ]
 })
 export class LoginModule { }
---
+++ src/app/sharedmodule/sharedmodule.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatSelectModule } from '@angular/material/select';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule, MatCardModule, MatAutocompleteModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    DragDropModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule,
+    MatSliderModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    DragDropModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule,
+    MatSliderModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatPaginatorModule
+  ]
+})
+export class SharedmoduleModule { }
