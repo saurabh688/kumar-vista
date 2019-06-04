fceeffcf402f1acdfb6a841b5098c6d97207d46c
Saurabh Rai <saurabh.rai@crecientech.com>
2019-06-04 16:12:57 +0530

    flat details updated.
    flat details updated.

--- src/app/flat-details/flat-details.component.ts
+++ src/app/flat-details/flat-details.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flat-details.component.scss']
 })
 export class FlatDetailsComponent implements OnInit {
-
+  viewPaidDetails = true;
+  modeList = [{ name: 'Check', value: 'check' }, { name: 'Cash', value: 'cash' }]
+  detailsList = [{name: 'Raj', mode: 'check', receivedDate: '20/05/2019',
+                  amount: 1000, checkNo: '100000013213', checkDate: '22/05/2019',
+                  bankName: 'SBI', depositedBankName: 'Axis'}];
+  dateType = "text";
+  editList = 'view';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  print() {
+
+  }
+
+  AddItem() {
+    this.detailsList.push({name: null, mode: null, receivedDate: null,
+    amount: null, checkNo: null, checkDate: null,
+    bankName: null, depositedBankName: null});
+  }
+  changeDateType() {
+    this.dateType = 'date';
+  }
+
+  saveEditScreen() {
+    this.editList = 'view';
+  }
+
+  deleteItem(index) {
+    this.detailsList.splice(index,1);
   }
 
 }
